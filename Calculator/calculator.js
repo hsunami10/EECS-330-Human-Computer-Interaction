c974a7b4529f62a108f9130bbe8a7e06198472c3
Michael Hsu <michael100.hsu@gmail.com>
2017-01-26 15:33:07 -0600

    finalized implementation of calculator

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,14 +1,46 @@
 var append = false;
 var negative = false;
-var zero = true;
+var countOp = 0;
+var begin = true;
+var value = 0;
+var operator = "";
+
+var operators = function(operate) {
+	document.getElementById("op").innerHTML = operate.target.id;
+	if(operate.target.id != operator && operator.length !== 0)
+		operator = operate.target.id;
+	if(countOp > 0)
+		return;
+	if(begin)
+		value = parseInt(interface.innerHTML);
+	else if(operator == "+")
+		value += parseInt(interface.innerHTML);
+	else if(operator == "-")
+		value -= parseInt(interface.innerHTML);
+	else if(operator == "x")
+		value *= parseInt(interface.innerHTML);
+	else if(operator == "÷")
+		value /= parseInt(interface.innerHTML);
+	else
+		value = parseInt(interface.innerHTML);
+	countOp++;
+	interface.innerHTML = value.toString();
+	begin = false;
+	operator = operate.target.id;
+	append = false;
+};
 
 window.onload = function() {
 	var interface = document.getElementById("interface");
+	var op = document.getElementById("op");
 
 	// Clear button
 	document.getElementById("C").onclick = function(e) {
 		interface.innerHTML = '0';
 		append = false;
+		begin = true;
+		value = 0;
+		operator = "";
 	};
 
 	// Decimal button
@@ -31,6 +63,37 @@ window.onload = function() {
 		}
 	};
 
+	// Operators
+	document.getElementById("+").onclick = function(e) {
+		operators(e);
+	};
+	document.getElementById("-").onclick = function(e) {
+		operators(e);
+	};
+	document.getElementById("x").onclick = function(e) {
+		operators(e);
+	};
+	document.getElementById("÷").onclick = function(e) {
+		operators(e);
+	};
+
+	// Equal
+	document.getElementById("=").onclick = function(e) {
+		if(operator == "+")
+			value += parseInt(interface.innerHTML);
+		else if(operator == "-")
+			value -= parseInt(interface.innerHTML);
+		else if(operator == "x")
+			value *= parseInt(interface.innerHTML);
+		else if(operator == "÷")
+			value /= parseInt(interface.innerHTML);
+
+		interface.innerHTML = value.toString();
+		begin = false;
+		operator = "";
+		append = false;
+	};
+
 	// Numbers
 	for(var i = 0; i < 10; i++) {
 		var button = document.getElementById(i);
@@ -42,7 +105,7 @@ window.onload = function() {
       				interface.innerHTML += e.target.id;
       			else
       				interface.innerHTML = e.target.id;
-
+      			countOp = 0;
       			append = true;
       		};
 		}
