16a67e7dc10edf9842446e22f355eee23cd289df
Michael Hsu <michael100.hsu@gmail.com>
2017-02-07 14:54:23 -0600

    made web app responsive to screen sizes

--- Web Application Project/public/user.js
+++ Web Application Project/public/user.js
@@ -1,8 +1,63 @@
 // Javascript file for User Interaction and JQuery
+var screenWidth = $(window).width();
+var screenHeight = $(window).height();
+
 $(document).ready(function() {
+	// Get browser window width and height
+
+	// Main menu
 	$('#menu_button').on({
-		hover: function() {
-			$(this).css('background-color', 'lightgray');
+		mouseenter: function() {
+			$('#menu_button div').animate({
+				backgroundColor: '#D3D3D3'
+			}, 200);
+		},
+		mouseleave: function() {
+			$('#menu_button div').animate({
+				backgroundColor: 'white'
+			}, 200);
+		},
+		mousedown: function() {
+			$('#menu_button div').css('background-color', '#707070');
+		},
+		mouseup: function() {
+			$('#mySidenav').animate({
+				width: '250px'
+			});
+			$('#main').animate({
+				marginLeft: '250px'
+			});
+			$('body').animate({
+				backgroundColor: '#b28443'
+			});
+			$('#menu_button div').css('background-color', 'white');
+			$('li').animate({
+				marginLeft: '5%',
+				marginRight: '5%'
+			});
+		}
+	});
+	$('#closeNavButton').click(function() {
+		$('#mySidenav').animate({
+			width: '0px'
+		});
+		$('#main').animate({
+			marginLeft: '0px'
+		});
+		$('body').animate({
+			backgroundColor: '#F2B35C'
+		});
+		$('li').animate({
+			marginLeft: '7.2%',
+			marginRight: '7.2%'
+		});
+	});
+	$('.sidenav a').on({
+		mouseenter: function() {
+			$(this).animate({color: 'white'},250);
+		},
+		mouseleave: function() {
+			$(this).animate({color: '#818181'},250);
 		}
 	});
 });
--- Web Application Project/server.js
+++ Web Application Project/server.js
@@ -9,8 +9,20 @@ var server = http.createServer(app);
 app.use(express.static(path.join(__dirname + '/public')));
 
 // Routing
-app.get("/", function(req, res) {
+app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/mainmenu.html'));
+});
+app.get('/mainmenu.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/mainmenu.html'));
+});
+app.get('/settings.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/settings.html'));
+});
+app.get('/friends.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/friends.html'));
+});
+app.get('/statistics.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/statistics.html'));
 });
 
 server.listen(1337, function() {
