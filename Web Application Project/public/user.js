a86e252858ce579d25e7db53d1dde764cf80f656
Michael Hsu <michael100.hsu@gmail.com>
2017-02-06 18:50:51 -0600

    started final project

---
+++ Web Application Project/public/user.js
@@ -0,0 +1,8 @@
+// Javascript file for User Interaction and JQuery
+$(document).ready(function() {
+	$('#menu_button').on({
+		hover: function() {
+			$(this).css('background-color', 'lightgray');
+		}
+	});
+});
--- Web Application Project/server.js
+++ Web Application Project/server.js
@@ -2,12 +2,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var http = require('http');
+var server = http.createServer(app);
+
+// Use middleware
+app.use(express.static(path.join(__dirname + '/public')));
 
 // Routing
 app.get("/", function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.sendFile(path.join(__dirname + '/mainmenu.html'));
 });
 
-app.listen(1337, function() {
+server.listen(1337, function() {
 	console.log('Server listening on port 1337');
-}); 
+});
