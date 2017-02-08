4eb411d49a174ffb0063ea57fcdd5d7274e1fe91
Michael Hsu <michael100.hsu@gmail.com>
2017-02-08 11:04:28 -0600

    finished all progress and favorites links

--- Web Application Project/server.js
+++ Web Application Project/server.js
@@ -24,8 +24,32 @@ app.get('/friends.html', function(req, res) {
 app.get('/statistics.html', function(req, res) {
 	res.sendFile(path.join(__dirname + '/statistics.html'));
 });
-app.get('/progress.html', function(req, res) {
-	res.sendFile(path.join(__dirname + '/progress.html'));
+app.get('/progressday.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/progressday.html'));
+});
+app.get('/progressweek.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/progressweek.html'));
+});
+app.get('/progressmonth.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/progressmonth.html'));
+});
+app.get('/progressyear.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/progressyear.html'));
+});
+app.get('/calendar.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/calendar.html'));
+});
+app.get('/favoritesday.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/favoritesday.html'));
+});
+app.get('/favoritesweek.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/favoritesweek.html'));
+});
+app.get('/favoritesmonth.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/favoritesmonth.html'));
+});
+app.get('/favoritesyear.html', function(req, res) {
+	res.sendFile(path.join(__dirname + '/favoritesyear.html'));
 });
 
 server.listen(1337, function() {
